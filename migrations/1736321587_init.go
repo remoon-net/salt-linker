6fe37a043838ceacf0ca4a26602e655c77ee7aea
shynome <shynome@gmail.com>
2025-01-12 16:58:04 +0800

    设置为系统字段以免字段被错误删除影响程序运行

--- migrations/1736321587_init.go
+++ migrations/1736321587_init.go
@@ -29,36 +29,32 @@ func init() {
 		try.To(app.Save(users))
 
 		endpoints := core.NewBaseCollection("endpoints", "__endpoints__")
+		endpoints.System = true
 		endpoints.ListRule = types.Pointer("user = @request.auth.id")
 		endpoints.ViewRule = types.Pointer("user = @request.auth.id")
 		endpoints.Fields.Add(
 			&core.RelationField{
-				Id:            "__user__",
-				Name:          "user",
+				Id: "__user__", Name: "user", System: true,
 				CascadeDelete: true,
 				Required:      true,
 				CollectionId:  users.Id,
 				MaxSelect:     1,
 			},
 			&core.NumberField{
-				Id:      "__transmit_bytes__",
-				Name:    "transmit_bytes",
+				Id: "__transmit_bytes__", Name: "transmit_bytes", System: true,
 				OnlyInt: true,
 				Min:     types.Pointer[float64](0),
 			},
 			&core.TextField{
-				Id:       "__token__",
-				Name:     "token",
+				Id: "__token__", Name: "token", System: true,
 				Required: true,
 			},
 			&core.AutodateField{
-				Id:       "__created__",
-				Name:     "created",
+				Id: "__created__", Name: "created", System: true,
 				OnCreate: true,
 			},
 			&core.AutodateField{
-				Id:       "__updated__",
-				Name:     "updated",
+				Id: "__updated__", Name: "updated", System: true,
 				OnCreate: true,
 				OnUpdate: true,
 			},
@@ -66,6 +62,7 @@ func init() {
 		try.To(app.Save(endpoints))
 
 		devices := core.NewBaseCollection("devices", "__devices__")
+		devices.System = true
 		devices.ListRule = types.Pointer("user = @request.auth.id")
 		devices.ViewRule = types.Pointer("user = @request.auth.id")
 		devices.CreateRule = types.Pointer("user = @request.auth.id")
@@ -74,34 +71,29 @@ func init() {
 		devices.DeleteRule = types.Pointer("user = @request.auth.id")
 		devices.Fields.Add(
 			&core.RelationField{
-				Id:            "__user__",
-				Name:          "user",
+				Id: "__user__", Name: "user", System: true,
 				CascadeDelete: true,
 				Required:      true,
 				CollectionId:  users.Id,
 				MaxSelect:     1,
 			},
 			&core.TextField{
-				Id:          "__name__",
-				Name:        "name",
+				Id: "__name__", Name: "name", System: true,
 				Required:    true,
 				Max:         200,
 				Presentable: true,
 			},
 			&core.RelationField{
-				Id:           "__endpoint__",
-				Name:         "endpoint",
+				Id: "__endpoint__", Name: "endpoint", System: true,
 				CollectionId: endpoints.Id,
 				MaxSelect:    1,
 			},
 			&core.AutodateField{
-				Id:       "__created__",
-				Name:     "created",
+				Id: "__created__", Name: "created", System: true,
 				OnCreate: true,
 			},
 			&core.AutodateField{
-				Id:       "__updated__",
-				Name:     "updated",
+				Id: "__updated__", Name: "updated", System: true,
 				OnCreate: true,
 				OnUpdate: true,
 			},
@@ -118,47 +110,41 @@ func init() {
 		try.To(app.Save(endpoints))
 
 		connections := core.NewBaseCollection("connections", "__connections__")
+		connections.System = true
 		connections.ListRule = types.Pointer("user = @request.auth.id")
 		connections.ViewRule = types.Pointer("user = @request.auth.id")
 		connections.Fields.Add(
 			&core.RelationField{
-				Id:            "__user__",
-				Name:          "user",
+				Id: "__user__", Name: "user", System: true,
 				CascadeDelete: true,
 				Required:      true,
 				CollectionId:  users.Id,
 				MaxSelect:     1,
 			},
 			&core.RelationField{
-				Id:            "__endpoint__",
-				Name:          "endpoint",
+				Id: "__endpoint__", Name: "endpoint", System: true,
 				CascadeDelete: true,
 				Required:      true,
 				CollectionId:  endpoints.Id,
 				MaxSelect:     1,
 			},
 			&core.NumberField{
-				Id:      "__transmit_bytes__",
-				Name:    "transmit_bytes",
+				Id: "__transmit_bytes__", Name: "transmit_bytes", System: true,
 				OnlyInt: true,
 				Min:     types.Pointer[float64](0),
 			},
 			&core.DateField{
-				Id:   "__disconnected__",
-				Name: "disconnected",
+				Id: "__disconnected__", Name: "disconnected", System: true,
 			},
 			&core.JSONField{
-				Id:   "__metadata__",
-				Name: "metadata",
+				Id: "__metadata__", Name: "metadata", System: true,
 			},
 			&core.AutodateField{
-				Id:       "__created__",
-				Name:     "created",
+				Id: "__created__", Name: "created", System: true,
 				OnCreate: true,
 			},
 			&core.AutodateField{
-				Id:       "__updated__",
-				Name:     "updated",
+				Id: "__updated__", Name: "updated", System: true,
 				OnCreate: true,
 				OnUpdate: true,
 			},
