57a59658fc75bc49b703cb548954f74a25f3841a
shynome <shynome@gmail.com>
2025-06-09 20:49:50 +0800

    gen-license ok

---
+++ hookjs/gen-license.js
@@ -0,0 +1,38 @@
+#!/bin/node
+
+/// <reference path="./types.d.ts" />
+
+/**
+ * @typedef {Object} OrderItem
+ * @prop {string} id
+ * @prop {string} user
+ * @prop {string} order
+ * @prop {string} goods
+ * @prop {number} num
+ */
+
+/**
+ * @typedef {Object} Order
+ * @prop {string} id
+ * @prop {{[k:string]:{pubkey:string}}} address
+ */
+
+define(() => {
+  const express = $app.findCollectionByNameOrId("express")
+  /**
+   * @param {core.Record} item
+   */
+  return function cb(item) {
+    let order = $app.findRecordById("orders", item.getString("order"))
+    let addrs = JSON.parse(order.getString("address"))
+    let addr = addrs[item.id]
+    let l = GenLicense(addr.pubkey)
+    let ex = new Record(express)
+    ex.set("user", item.getString("user"))
+    ex.set("order", item.getString("order"))
+    ex.set("items", [item.id])
+    ex.set("value", JSON.stringify({ license: l }))
+    ex.set("remark", "salt-weblink的许可签名")
+    $app.save(ex)
+  }
+})
