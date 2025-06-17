709e3aa3f44d69da052e04f1d0e9c46f78127dc2
shynome <shynome@gmail.com>
2025-06-17 11:47:34 +0800

    express 也要添加到 order 中

--- hookjs/gen-license.js
+++ hookjs/gen-license.js
@@ -23,16 +23,22 @@ define(() => {
    * @param {core.Record} item
    */
   return function cb(item) {
-    let order = $app.findRecordById("orders", item.getString("order"))
-    let addrs = JSON.parse(order.getString("address"))
-    let addr = addrs[item.id]
-    let l = GenLicense(addr.pubkey)
-    let ex = new Record(express)
-    ex.set("user", item.getString("user"))
-    ex.set("order", item.getString("order"))
-    ex.set("items", [item.id])
-    ex.set("value", JSON.stringify({ license: l }))
-    ex.set("remark", "salt-weblink的许可签名")
-    $app.save(ex)
+    $app.runInTransaction((tx) => {
+      let order = tx.findRecordById("orders", item.getString("order"))
+      let addrs = JSON.parse(order.getString("address"))
+      let addr = addrs[item.id]
+      let l = GenLicense(addr.pubkey)
+      let ex = new Record(express)
+      ex.set("user", item.getString("user"))
+      ex.set("order", item.getString("order"))
+      ex.set("items", [item.id])
+      ex.set("value", JSON.stringify({ license: l }))
+      ex.set("remark", "salt-weblink的许可签名")
+      tx.save(ex)
+      let exs = order.getStringSlice("express")
+      exs.push(ex.id)
+      order.set("ex", exs)
+      tx.save(order)
+    })
   }
 })
